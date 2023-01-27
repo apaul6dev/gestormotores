690ad4352cae7999afa8c5aae0260a4460d2d3da
Paul Gallegos Aguilar <apaul.gallegos@gmail.com>
2023-01-27 10:17:45 -0500

    Correcion uri

--- src/main/java/com/grapes/mmotor/controller/UsuarioController.java
+++ src/main/java/com/grapes/mmotor/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 @RestController
-@RequestMapping("/empresas")
+@RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
     private IUsuarioService service;
