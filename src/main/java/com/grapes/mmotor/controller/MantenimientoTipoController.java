1cd78b3c36370856755c57062df73065b8617be7
Andres Gallegos Aguilar <andres.gallegos1611@gmail.com>
2023-01-27 11:36:12 -0500

    implementacion del servicio de obtener eliminar y agregar todos los mantenimientos tipo

--- src/main/java/com/grapes/mmotor/controller/MantenimientoTipoController.java
+++ src/main/java/com/grapes/mmotor/controller/MantenimientoTipoController.java
@@ -1,15 +1,18 @@
 package com.grapes.mmotor.controller;
 
+import com.grapes.mmotor.exception.ModeloNotFoundException;
+import com.grapes.mmotor.model.Empresa;
 import com.grapes.mmotor.model.MantenimientoTipo;
 import com.grapes.mmotor.service.IMantenimientoTipoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,4 +31,43 @@ public class MantenimientoTipoController {
 
     }
 
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MantenimientoTipo> listarId(@PathVariable("id") Integer id) {
+        MantenimientoTipo dato = new MantenimientoTipo();
+        dato = service.listarId(id);
+        if (dato == null) {
+            throw new ModeloNotFoundException("ID: " + id);
+        }
+        return new ResponseEntity<MantenimientoTipo>(dato, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> registrar(@Validated @RequestBody MantenimientoTipo data) {
+        MantenimientoTipo tmp = new MantenimientoTipo();
+        service.registrar(data);
+        tmp = service.listarId(data.getIdMantenimientoTipo());
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(tmp.getIdMantenimientoTipo())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void eliminar(@PathVariable("id") Integer id) {
+        MantenimientoTipo exa = service.listarId(id);
+        if (exa == null) {
+            throw new ModeloNotFoundException("ID: " + id);
+        } else {
+            service.eliminar(id);
+        }
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> actualizar(@Validated @RequestBody MantenimientoTipo data) {
+        service.modificar(data);
+        return new ResponseEntity<Object>(HttpStatus.OK);
+    }
 }
+
+
